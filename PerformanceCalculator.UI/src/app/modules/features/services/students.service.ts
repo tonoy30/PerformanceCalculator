026a03984f6e9ea9bef631bc205aab4a8e1fd0ca
masum_mollik <masum.mollik@orbitax.com>
2021-03-20 18:12:20 +0600

    Merge branch 'main' of https://github.com/tonoy30/advance-database-lab
    # Conflicts:
    #	PerformanceCalculator.UI/src/app/modules/features/services/students.service.ts

--- PerformanceCalculator.UI/src/app/modules/features/services/students.service.ts
+++ PerformanceCalculator.UI/src/app/modules/features/services/students.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import {Student} from "../../../models/student";
 
 @Injectable({
   providedIn: 'root',
@@ -25,5 +26,10 @@ export class StudentsService {
 
   deleteStudent(id: string) {
     return this.http.delete(`${environment.apiUrl}/student/${id}`);
+  }
+  getStudentByCourseId(id: string) {
+    return this.http.get<any[]>(
+      `${environment.apiUrl}/Student/students/${id}`
+    );
   }
 }
