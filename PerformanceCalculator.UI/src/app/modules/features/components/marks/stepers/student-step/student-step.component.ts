c77bffd9b12ef123a1d34ddfe478170c74322cc5
tonoy.akando <tonoy.akando@orbitax.com>
2021-03-20 15:59:00 +0600

    steper

--- PerformanceCalculator.UI/src/app/modules/features/components/marks/stepers/student-step/student-step.component.ts
+++ PerformanceCalculator.UI/src/app/modules/features/components/marks/stepers/student-step/student-step.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-student-step',
-  templateUrl: './student-step.component.html',
-  styleUrls: ['./student-step.component.scss']
+	selector: "app-student-step",
+	templateUrl: "./student-step.component.html",
+	styleUrls: ["./student-step.component.scss"],
 })
 export class StudentStepComponent implements OnInit {
+	constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
+	nextPage() {
+		this.router.navigate(["marks/obtained-mark"]);
+	}
+	prevPage() {
+		this.router.navigate(["marks/course"]);
+	}
 }
